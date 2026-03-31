d3dfd6419f5c33e26f8439b3e91e41d2dd233a19
Mahmoudali55 <mahmoud7491538@std.mans.edu.eg>
2026-03-31 10:04:00 +0300

    done_refactor_Bank_Offer

--- lib/features/cars/presentation/screen/bank_offers_screen.dart
+++ lib/features/cars/presentation/screen/bank_offers_screen.dart
@@ -1,12 +1,9 @@
 import 'package:car/core/localization/app_locale_keys.dart';
 import 'package:car/core/theme/app_colors.dart';
 import 'package:car/features/cars/presentation/widget/bank_offer_card_widget.dart';
+import 'package:car/features/cars/presentation/widget/bank_offers_widgets.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:gap/gap.dart';
-
-enum SortOption { lowestMargin, highestMargin, lowestInstallment }
 
 class BankOffersScreen extends StatefulWidget {
   final Map<String, dynamic> car;
@@ -114,215 +111,40 @@ class _BankOffersScreenState extends State<BankOffersScreen> {
         physics: const BouncingScrollPhysics(),
         slivers: [
           SliverToBoxAdapter(
-            child: _buildCalculatorSection(context),
+            child: BankOfferCalculatorCard(
+              carPrice: _carPrice,
+              downPayment: _downPayment,
+              durationYears: _durationYears,
+              downPaymentController: _downPaymentController,
+              onDownPaymentChanged: (value) {
+                setState(() {
+                  _downPayment = double.tryParse(value) ?? 0;
+                });
+              },
+              onDurationChanged: (value) {
+                setState(() {
+                  _durationYears = value.toInt();
+                });
+              },
+            ),
           ),
           SliverToBoxAdapter(
-            child: _buildFilterSection(context),
-          ),
-          SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-            sliver: SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  final offer = _sortedOffers[index];
-                  return BankOfferCardWidget(
-                    offer: offer,
-                    carPrice: _carPrice,
-                    downPayment: _downPayment,
-                    durationYears: _durationYears,
-                  );
-                },
-                childCount: _sortedOffers.length,
-              ),
+            child: BankOfferFilterSection(
+              currentSort: _currentSort,
+              onSortChanged: (option) {
+                setState(() {
+                  _currentSort = option;
+                });
+              },
             ),
+          ),
+          BankOffersList(
+            sortedOffers: _sortedOffers,
+            carPrice: _carPrice,
+            downPayment: _downPayment,
+            durationYears: _durationYears,
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildCalculatorSection(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(20.w),
-      padding: EdgeInsets.all(20.w),
-      decoration: BoxDecoration(
-        color: AppColor.secondAppColor(context),
-        borderRadius: BorderRadius.circular(16.r),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.05),
-            blurRadius: 10,
-            offset: const Offset(0, 5),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                '${AppLocaleKey.totalAmount.tr()}:',
-                style: TextStyle(color: Colors.grey[600], fontSize: 14.sp),
-              ),
-              Text(
-                '${NumberFormat('#,##0', 'en_US').format(_carPrice)} SAR',
-                style: TextStyle(
-                  color: AppColor.blackTextColor(context),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.sp,
-                ),
-              ),
-            ],
-          ),
-          Gap(20.h),
-          Text(
-            AppLocaleKey.downPayment.tr(),
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 14.sp,
-              color: AppColor.blackTextColor(context),
-            ),
-          ),
-          Gap(8.h),
-          TextField(
-            controller: _downPaymentController,
-            keyboardType: TextInputType.number,
-            style: TextStyle(color: AppColor.blackTextColor(context)),
-            decoration: InputDecoration(
-              hintText: '0 SAR',
-              hintStyle: TextStyle(color: Colors.grey[400]),
-              filled: true,
-              fillColor: AppColor.scaffoldColor(context),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12.r),
-                borderSide: BorderSide.none,
-              ),
-              contentPadding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
-            ),
-            onChanged: (value) {
-              setState(() {
-                _downPayment = double.tryParse(value) ?? 0;
-              });
-            },
-          ),
-          Gap(20.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                AppLocaleKey.durationInYears.tr(),
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14.sp,
-                  color: AppColor.blackTextColor(context),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
-                decoration: BoxDecoration(
-                  color: AppColor.primaryColor(context).withValues(alpha: 0.1),
-                  borderRadius: BorderRadius.circular(8.r),
-                ),
-                child: Text(
-                  '$_durationYears',
-                  style: TextStyle(
-                    color: AppColor.primaryColor(context),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14.sp,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Slider(
-            value: _durationYears.toDouble(),
-            min: 1,
-            max: 5,
-            divisions: 4,
-            activeColor: AppColor.primaryColor(context),
-            inactiveColor: Colors.grey.withValues(alpha: 0.2),
-            onChanged: (value) {
-              setState(() {
-                _durationYears = value.toInt();
-              });
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildFilterSection(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20.w),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            AppLocaleKey.sortBy.tr(),
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 14.sp,
-              color: AppColor.blackTextColor(context),
-            ),
-          ),
-          Gap(12.h),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            physics: const BouncingScrollPhysics(),
-            child: Row(
-              children: [
-                _buildSortChip(AppLocaleKey.lowestMargin.tr(), SortOption.lowestMargin, context),
-                Gap(12.w),
-                _buildSortChip(AppLocaleKey.highestMargin.tr(), SortOption.highestMargin, context),
-                Gap(12.w),
-                _buildSortChip(AppLocaleKey.lowestInstallment.tr(), SortOption.lowestInstallment, context),
-              ],
-            ),
-          ),
-          Gap(10.h),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildSortChip(String label, SortOption option, BuildContext context) {
-    final isSelected = _currentSort == option;
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _currentSort = option;
-        });
-      },
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.h),
-        decoration: BoxDecoration(
-          color: isSelected ? AppColor.primaryColor(context) : AppColor.secondAppColor(context),
-          borderRadius: BorderRadius.circular(20.r),
-          border: Border.all(
-            color: isSelected ? Colors.transparent : Colors.grey.withValues(alpha: 0.2),
-          ),
-          boxShadow: isSelected
-              ? [
-                  BoxShadow(
-                    color: AppColor.primaryColor(context).withValues(alpha: 0.3),
-                    blurRadius: 8,
-                    offset: const Offset(0, 3),
-                  )
-                ]
-              : null,
-        ),
-        child: Text(
-          label,
-          style: TextStyle(
-            color: isSelected ? AppColor.whiteColor(context) : Colors.grey[600],
-            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-            fontSize: 13.sp,
-          ),
-        ),
       ),
     );
   }
---
+++ lib/features/cars/presentation/widget/bank_offers_widgets.dart
@@ -0,0 +1,281 @@
+import 'package:car/core/localization/app_locale_keys.dart';
+import 'package:car/core/theme/app_colors.dart';
+import 'package:car/features/cars/presentation/widget/bank_offer_card_widget.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+
+enum SortOption { lowestMargin, highestMargin, lowestInstallment }
+
+class BankOfferCalculatorCard extends StatelessWidget {
+  final num carPrice;
+  final double downPayment;
+  final int durationYears;
+  final TextEditingController downPaymentController;
+  final ValueChanged<String> onDownPaymentChanged;
+  final ValueChanged<double> onDurationChanged;
+
+  const BankOfferCalculatorCard({
+    super.key,
+    required this.carPrice,
+    required this.downPayment,
+    required this.durationYears,
+    required this.downPaymentController,
+    required this.onDownPaymentChanged,
+    required this.onDurationChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.w),
+      padding: EdgeInsets.all(20.w),
+      decoration: BoxDecoration(
+        color: AppColor.secondAppColor(context),
+        borderRadius: BorderRadius.circular(16.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.05),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '${AppLocaleKey.totalAmount.tr()}:',
+                style: TextStyle(color: Colors.grey[600], fontSize: 14.sp),
+              ),
+              Text(
+                '${NumberFormat('#,##0', 'en_US').format(carPrice)} ${AppLocaleKey.sar.tr()}',
+                style: TextStyle(
+                  color: AppColor.blackTextColor(context),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.sp,
+                ),
+              ),
+            ],
+          ),
+          Gap(20.h),
+          Text(
+            AppLocaleKey.downPayment.tr(),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14.sp,
+              color: AppColor.blackTextColor(context),
+            ),
+          ),
+          Gap(8.h),
+          TextField(
+            controller: downPaymentController,
+            keyboardType: TextInputType.number,
+            style: TextStyle(color: AppColor.blackTextColor(context)),
+            decoration: InputDecoration(
+              hintText: '0 ${AppLocaleKey.sar.tr()}',
+              hintStyle: TextStyle(color: Colors.grey[400]),
+              filled: true,
+              fillColor: AppColor.scaffoldColor(context),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.r),
+                borderSide: BorderSide.none,
+              ),
+              contentPadding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
+            ),
+            onChanged: onDownPaymentChanged,
+          ),
+          Gap(20.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                AppLocaleKey.durationInYears.tr(),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14.sp,
+                  color: AppColor.blackTextColor(context),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 4.h),
+                decoration: BoxDecoration(
+                  color: AppColor.primaryColor(context).withValues(alpha: 0.1),
+                  borderRadius: BorderRadius.circular(8.r),
+                ),
+                child: Text(
+                  '$durationYears',
+                  style: TextStyle(
+                    color: AppColor.primaryColor(context),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14.sp,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Slider(
+            value: durationYears.toDouble(),
+            min: 1,
+            max: 5,
+            divisions: 4,
+            activeColor: AppColor.primaryColor(context),
+            inactiveColor: Colors.grey.withValues(alpha: 0.2),
+            onChanged: onDurationChanged,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BankOfferFilterSection extends StatelessWidget {
+  final SortOption currentSort;
+  final ValueChanged<SortOption> onSortChanged;
+
+  const BankOfferFilterSection({
+    super.key,
+    required this.currentSort,
+    required this.onSortChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            AppLocaleKey.sortBy.tr(),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14.sp,
+              color: AppColor.blackTextColor(context),
+            ),
+          ),
+          Gap(12.h),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            physics: const BouncingScrollPhysics(),
+            child: Row(
+              children: [
+                BankOfferSortChip(
+                  label: AppLocaleKey.lowestMargin.tr(),
+                  option: SortOption.lowestMargin,
+                  isSelected: currentSort == SortOption.lowestMargin,
+                  onTap: () => onSortChanged(SortOption.lowestMargin),
+                ),
+                Gap(12.w),
+                BankOfferSortChip(
+                  label: AppLocaleKey.highestMargin.tr(),
+                  option: SortOption.highestMargin,
+                  isSelected: currentSort == SortOption.highestMargin,
+                  onTap: () => onSortChanged(SortOption.highestMargin),
+                ),
+                Gap(12.w),
+                BankOfferSortChip(
+                  label: AppLocaleKey.lowestInstallment.tr(),
+                  option: SortOption.lowestInstallment,
+                  isSelected: currentSort == SortOption.lowestInstallment,
+                  onTap: () => onSortChanged(SortOption.lowestInstallment),
+                ),
+              ],
+            ),
+          ),
+          Gap(10.h),
+        ],
+      ),
+    );
+  }
+}
+
+class BankOfferSortChip extends StatelessWidget {
+  final String label;
+  final SortOption option;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const BankOfferSortChip({
+    super.key,
+    required this.label,
+    required this.option,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.h),
+        decoration: BoxDecoration(
+          color: isSelected ? AppColor.primaryColor(context) : AppColor.secondAppColor(context),
+          borderRadius: BorderRadius.circular(20.r),
+          border: Border.all(
+            color: isSelected ? Colors.transparent : Colors.grey.withValues(alpha: 0.2),
+          ),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: AppColor.primaryColor(context).withValues(alpha: 0.3),
+                    blurRadius: 8,
+                    offset: const Offset(0, 3),
+                  )
+                ]
+              : null,
+        ),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: isSelected ? AppColor.whiteColor(context) : Colors.grey[600],
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+            fontSize: 13.sp,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BankOffersList extends StatelessWidget {
+  final List<BankOffer> sortedOffers;
+  final num carPrice;
+  final double downPayment;
+  final int durationYears;
+
+  const BankOffersList({
+    super.key,
+    required this.sortedOffers,
+    required this.carPrice,
+    required this.downPayment,
+    required this.durationYears,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            final offer = sortedOffers[index];
+            return BankOfferCardWidget(
+              offer: offer,
+              carPrice: carPrice,
+              downPayment: downPayment,
+              durationYears: durationYears,
+            );
+          },
+          childCount: sortedOffers.length,
+        ),
+      ),
+    );
+  }
+}
