d95733a10e5a52c6dc8e01851c1239f007c79d38
Mahmoudali55 <mahmoud7491538@std.mans.edu.eg>
2026-07-08 09:15:54 +0300

    chore: remove unnecessary comment in main function

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  // Initialize services
+
   await ServiceInitialize.initialize();
   final String lang = HiveMethods.getLang();
   //
