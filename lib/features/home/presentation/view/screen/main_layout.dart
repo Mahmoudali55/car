bceb17cda525631579988f1ef69a2eca8e8fb3f8
asggroups56-svg <asggroups56@gmail.com>
2026-08-24 13:17:19 +0300

    done

--- lib/features/home/presentation/view/screen/main_layout.dart
+++ lib/features/home/presentation/view/screen/main_layout.dart
@@ -2,22 +2,21 @@ import 'package:car/core/custom_widgets/custom_app_bar/custom_app_bar.dart';
 import 'package:car/core/localization/app_locale_keys.dart';
 import 'package:car/core/theme/app_text_style.dart';
 import 'package:car/features/favorites/presentation/view/screen/favorites_screen.dart';
+import 'package:car/features/home/presentation/cubit/home_cubit.dart';
 import 'package:car/features/home/presentation/view/widgets/cart_icon_widget.dart';
 import 'package:car/features/home/presentation/view/widgets/notification_icon_widget.dart';
 import 'package:car/features/home/presentation/view/widgets/profile_icon_widget.dart';
 import 'package:car/features/home/presentation/view/widgets/search_icon_widget.dart';
 import 'package:car/features/home/presentation/view/widgets/support_icon_widget.dart';
 import 'package:car/features/home/presentation/view/widgets/translate_icon_widget.dart';
-import 'package:car/features/home/presentation/cubit/home_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import '../../../../cars/presentation/screen/cars_screen.dart';
-
 import '../../../../services/presentation/screen/services_screen.dart';
 import '../widgets/main_bottom_nav_bar.dart';
 import 'guest_home_screen.dart';
